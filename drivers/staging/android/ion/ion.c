2778ebb5faf4a217a972200868201b0c83912712
Liam Mark <lmark@codeaurora.org>
2017-11-21 10:46:38 -0800

    ion: add kernel support to get buffer flags
    Allow ION kernel clients to get an ion buffer's flags.
    This will allow ION clients to be able to determine if a buffer
    is cached, uncached, secure ect.
    
    Change-Id: I5460478e69f28e74906fd298f57dabd9e62c1e1f
    Signed-off-by: Liam Mark <lmark@codeaurora.org>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -358,7 +358,16 @@ static void ion_dma_buf_end_cpu_access(struct dma_buf *dmabuf, size_t start,
 	__ion_unmap_kernel(buffer);
 }
 
-static const struct dma_buf_ops ion_dma_buf_ops = {
+static int ion_dma_buf_get_flags(struct dma_buf *dmabuf,
+				 unsigned long *flags)
+{
+	struct ion_buffer *buffer = dmabuf->priv;
+	*flags = buffer->flags;
+
+ 	return 0;
+}
+
+static struct dma_buf_ops dma_buf_ops = {
 	.map_dma_buf = ion_map_dma_buf,
 	.unmap_dma_buf = ion_unmap_dma_buf,
 	.mmap = ion_mmap,
@@ -366,7 +375,10 @@ static const struct dma_buf_ops ion_dma_buf_ops = {
 	.begin_cpu_access = ion_dma_buf_begin_cpu_access,
 	.end_cpu_access = ion_dma_buf_end_cpu_access,
 	.kmap_atomic = ion_dma_buf_kmap,
-	.kmap = ion_dma_buf_kmap
+	.kunmap_atomic = ion_dma_buf_kunmap,
+	.kmap = ion_dma_buf_kmap,
+	.get_flags = ion_dma_buf_get_flags,
+	.kunmap = ion_dma_buf_kunmap,
 };
 
 struct dma_buf *__ion_share_dma_buf(struct ion_buffer *buffer)
