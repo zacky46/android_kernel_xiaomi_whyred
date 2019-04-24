50bc92a591a547abbcbc9ad68ce5beb5bfdbb420
Nathan Chancellor <natechancellor@gmail.com>
2019-04-23 17:48:20 -0700

    drivers: soc: qcom: system_stats: Fix strlcpy usage
    Clang warns:
    
    ../drivers/soc/qcom/system_stats.c:381:13: warning: size argument in
    'strlcpy' call appears to be size of the source; expected the size of
    the destination [-Wstrlcpy-strlcat-size]
                                            strlen(master_name) + 1);
                                            ~~~~~~~^~~~~~~~~~~~~~~~
    
    Fixes: 1d321eb86c2f ("drivers: soc: qcom: system_stats: Support for System stats")
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/soc/qcom/system_stats.c
+++ drivers/soc/qcom/system_stats.c
@@ -378,9 +378,8 @@ static int msm_rpmstats_probe(struct platform_device *pdev)
 			pr_err("%s:Failed to get memory\n", __func__);
 			return -ENOMEM;
 		}
-		master_name_len = strlen(master_name);
-		strlcpy(ss.master[i], master_name,
-					master_name_len + 1);
+		strscpy(ss.master[i], master_name,
+					sizeof(ss.master[i]));
 	}
 
 	dent = debugfs_create_file("system_stats", S_IRUGO, NULL,
