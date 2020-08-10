e0a14a355e76a0b73f4d996196479591f32b6864
Parvinder Singh <psndna88@gmail.com>
2020-08-10 10:30:01 +0530

    camera: disable logging

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -23,8 +23,7 @@
 extern struct vendor_eeprom s_vendor_eeprom[CAMERA_VENDOR_EEPROM_COUNT_MAX];
 /* Logging macro */
 #undef CDBG
-#define CDBG(fmt, args...) pr_debug(fmt, ##args)
-#define LCT_CAMERA_DEBUG 0
+#define CDBG(fmt, args...)
 
 #define SENSOR_MAX_MOUNTANGLE (360)
 
